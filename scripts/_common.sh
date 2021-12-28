25f423db4f8577688f3535a5888b6c70f9eb0356
ericgaspar <junk.eg@free.fr>
2021-12-28 12:01:45 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="build-essential libpcre3 libpcre3-dev libssl-dev redis gunicorn3 uwsgi-plugin-python"
+pkg_dependencies="build-essential libpcre3 libpcre3-dev libssl-dev gunicorn3 uwsgi-plugin-python"
 
 #=================================================
 # PERSONAL HELPERS
